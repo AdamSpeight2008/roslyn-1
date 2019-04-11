bfb10c8fccecf7b5cf8ed098e5d05b8f3e8363ac
AdamSpeight2008 <adam.speight@virgin.net>
2019-04-11 18:13:12 +0100

    Use .ToListAndFree

--- src/Compilers/VisualBasic/Portable/Parser/ParseExpression.vb
+++ src/Compilers/VisualBasic/Portable/Parser/ParseExpression.vb
@@ -1126,8 +1126,7 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.Syntax.InternalSyntax
                 argumentBuilder.Add(SyntaxFactory.SimpleArgument(nameColonEquals:=Nothing, expression:=missing))
             End If
 
-            Dim arguments = argumentBuilder.ToList
-            _pool.Free(argumentBuilder)
+            Dim arguments = argumentBuilder.ToListAndFree(_pool)
 
             Dim tupleExpression = SyntaxFactory.TupleExpression(openParen, arguments, closeParen)
 
@@ -1298,8 +1297,7 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.Syntax.InternalSyntax
 
             Loop
 
-            Dim result = arguments.ToList
-            _pool.Free(arguments)
+            Dim result = arguments.ToListAndFree(_pool)
             Return result
 
         End Function
@@ -1698,7 +1696,7 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.Syntax.InternalSyntax
 
         Private Function ParseVariableList() As CodeAnalysis.Syntax.InternalSyntax.SeparatedSyntaxList(Of ExpressionSyntax)
 
-            Dim variables As SeparatedSyntaxListBuilder(Of ExpressionSyntax) = Me._pool.AllocateSeparated(Of ExpressionSyntax)()
+            Dim variables = Me._pool.AllocateSeparated(Of ExpressionSyntax)()
 
             Do
                 variables.Add(ParseVariable())
@@ -1711,8 +1709,7 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.Syntax.InternalSyntax
                 variables.AddSeparator(comma)
             Loop
 
-            Dim result = variables.ToList
-            Me._pool.Free(variables)
+            Dim result = variables.ToListAndFree(_pool)
 
             Return result
         End Function
