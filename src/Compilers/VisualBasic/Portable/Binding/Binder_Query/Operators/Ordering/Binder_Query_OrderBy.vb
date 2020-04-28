a8e643cd2a000cf843ad90e82219722902b5ff29
AdamSpeight2008 <adam.speight@virgin.net>
2020-04-28 08:57:13 +0100

    Separating out into its own function.

--- src/Compilers/VisualBasic/Portable/Binding/Binder_Query/Operators/Ordering/Binder_Query_OrderBy.vb
+++ src/Compilers/VisualBasic/Portable/Binding/Binder_Query/Operators/Ordering/Binder_Query_OrderBy.vb
@@ -50,74 +50,7 @@ Namespace Microsoft.CodeAnalysis.VisualBasic
             Dim keyBinder As QueryLambdaBinder = Nothing
 
             For i = 0 To orderByOrderings.Count - 1
-                Dim ordering As OrderingSyntax = orderByOrderings(i)
-
-                ' Create LambdaSymbol for the shape of the key lambda.
-                Dim param As BoundLambdaParameterSymbol = CreateQueryLambdaParameterSymbol(lambdaParameterName, 0,
-                                                                                           source.CompoundVariableType,
-                                                                                           ordering.Expression, source.RangeVariables)
-
-                Dim lambdaSymbol = Me.CreateQueryLambdaSymbol((SynthesizedLambdaKind.OrderingQueryLambda,LambdaUtilities.GetOrderingLambdaBody(ordering)),
-                                                              ImmutableArray.Create(param))
-
-                ' Create binder for a key expression.
-                keyBinder = New QueryLambdaBinder(lambdaSymbol, source.RangeVariables)
-
-                ' Bind expression as a value, conversion during overload resolution should take care of the rest (making it an RValue, etc.). 
-                Dim key As BoundExpression = keyBinder.BindValue(ordering.Expression, diagnostics)
-
-                Dim keyLambda = CreateBoundQueryLambda(lambdaSymbol,
-                                                       source.RangeVariables,
-                                                       key,
-                                                       exprIsOperandOfConditionalBranch:=False)
-
-                keyLambda.SetWasCompilerGenerated()
-
-                ' Now bind the call.
-                Dim boundCallOrBadExpression As BoundExpression
-
-                If sourceOrPreviousOrdering.Type.IsErrorType() Then
-                    boundCallOrBadExpression = BadExpression(ordering, ImmutableArray.Create(Of BoundExpression)(sourceOrPreviousOrdering, keyLambda),
-                                                             ErrorTypeSymbol.UnknownResultType).MakeCompilerGenerated()
-                Else
-                    If suppressDiagnostics Is Nothing AndAlso ShouldSuppressDiagnostics(keyLambda) Then
-                        ' Operator BindQueryClauseCall will fail, let's suppress any additional errors it will report.
-                        suppressDiagnostics = DiagnosticBag.GetInstance()
-                        callDiagnostics = suppressDiagnostics
-                    End If
-
-                    Dim operatorName As String
-                    Dim operatorNameLocation As TextSpan
-
-                    If i = 0 Then
-                        ' This is the first ordering.
-                        If ordering.Kind = SyntaxKind.AscendingOrdering Then
-                            operatorName = StringConstants.OrderByMethod
-                        Else
-                            Debug.Assert(ordering.Kind = SyntaxKind.DescendingOrdering)
-                            operatorName = StringConstants.OrderByDescendingMethod
-                        End If
-
-                        operatorNameLocation = GetQueryOperatorNameSpan(orderBy.OrderKeyword, orderBy.ByKeyword)
-                    Else
-                        If ordering.Kind = SyntaxKind.AscendingOrdering Then
-                            operatorName = StringConstants.ThenByMethod
-                        Else
-                            Debug.Assert(ordering.Kind = SyntaxKind.DescendingOrdering)
-                            operatorName = StringConstants.ThenByDescendingMethod
-                        End If
-
-                        operatorNameLocation = orderByOrderings.GetSeparator(i - 1).Span
-                    End If
-
-                    boundCallOrBadExpression = BindQueryOperatorCall(ordering, sourceOrPreviousOrdering,
-                                                                   operatorName,
-                                                                   ImmutableArray.Create(Of BoundExpression)(keyLambda),
-                                                                   operatorNameLocation,
-                                                                   callDiagnostics)
-                End If
-
-                sourceOrPreviousOrdering = New BoundOrdering(ordering, boundCallOrBadExpression, boundCallOrBadExpression.Type)
+                Internal_BindOrderByClause(source, orderBy, diagnostics, suppressDiagnostics, callDiagnostics, lambdaParameterName, sourceOrPreviousOrdering, orderByOrderings, keyBinder, i)
             Next
 
             If suppressDiagnostics IsNot Nothing Then suppressDiagnostics.Free()
@@ -132,6 +65,97 @@ Namespace Microsoft.CodeAnalysis.VisualBasic
                                         ImmutableArray.Create(Of Binder)(keyBinder),
                                         sourceOrPreviousOrdering.Type)
         End Function
+
+        Private Sub Internal_BindOrderByClause(
+                                                source As BoundQueryClauseBase,
+                                          ByRef orderBy As OrderByClauseSyntax,
+                                                diagnostics As DiagnosticBag,
+                                          ByRef suppressDiagnostics As DiagnosticBag,
+                                          ByRef callDiagnostics As DiagnosticBag,
+                                                lambdaParameterName As String,
+                                          ByRef sourceOrPreviousOrdering As BoundQueryPart,
+                                          ByRef orderByOrderings As SeparatedSyntaxList(Of OrderingSyntax),
+                                          ByRef keyBinder As QueryLambdaBinder,
+                                                i As Integer
+                                              )
+
+            Dim ordering As OrderingSyntax = orderByOrderings(i)
+
+            ' Create LambdaSymbol for the shape of the key lambda.
+            Dim param As BoundLambdaParameterSymbol = CreateQueryLambdaParameterSymbol(lambdaParameterName, 0,
+                                                                                       source.CompoundVariableType,
+                                                                                       ordering.Expression, source.RangeVariables)
+
+            Dim lambdaSymbol = Me.CreateQueryLambdaSymbol((SynthesizedLambdaKind.OrderingQueryLambda, LambdaUtilities.GetOrderingLambdaBody(ordering)),
+                                                          ImmutableArray.Create(param))
+
+            ' Create binder for a key expression.
+            keyBinder = New QueryLambdaBinder(lambdaSymbol, source.RangeVariables)
+
+            ' Bind expression as a value, conversion during overload resolution should take care of the rest (making it an RValue, etc.). 
+            Dim key As BoundExpression = keyBinder.BindValue(ordering.Expression, diagnostics)
+
+            Dim keyLambda = CreateBoundQueryLambda(lambdaSymbol,
+                                                   source.RangeVariables,
+                                                   key,
+                                                   exprIsOperandOfConditionalBranch:=False)
+
+            keyLambda.SetWasCompilerGenerated()
+
+            ' Now bind the call.
+            Dim boundCallOrBadExpression As BoundExpression
+
+            If sourceOrPreviousOrdering.Type.IsErrorType() Then
+                boundCallOrBadExpression = BadExpression(ordering, ImmutableArray.Create(Of BoundExpression)(sourceOrPreviousOrdering, keyLambda),
+                                                         ErrorTypeSymbol.UnknownResultType).MakeCompilerGenerated()
+            Else
+                If suppressDiagnostics Is Nothing AndAlso ShouldSuppressDiagnostics(keyLambda) Then
+                    ' Operator BindQueryClauseCall will fail, let's suppress any additional errors it will report.
+                    suppressDiagnostics = DiagnosticBag.GetInstance()
+                    callDiagnostics = suppressDiagnostics
+                End If
+
+                Dim [operator] = GetMethodNameOfOrderingClause(orderBy, orderByOrderings, i, ordering)
+
+                boundCallOrBadExpression = BindQueryOperatorCall(ordering, sourceOrPreviousOrdering,
+                                                               [operator].operatorName,
+                                                               ImmutableArray.Create(Of BoundExpression)(keyLambda),
+                                                               [operator].operatorNameLocation,
+                                                               callDiagnostics)
+            End If
+
+            sourceOrPreviousOrdering = New BoundOrdering(ordering, boundCallOrBadExpression, boundCallOrBadExpression.Type)
+        End Sub
+
+        Private Shared function GetMethodNameOfOrderingClause(
+                                                         ByRef orderBy As OrderByClauseSyntax,
+                                                         ByRef orderByOrderings As SeparatedSyntaxList(Of OrderingSyntax),
+                                                               i As Integer,
+                                                               ordering As OrderingSyntax
+                                                             ) As (operatorName As String, operatorNameLocation As TextSpan)
+            Dim result As (operatorName As String, operatorNameLocation As TextSpan) = (Nothing, Nothing)
+            If i = 0 Then
+                ' This is the first ordering.
+                If ordering.Kind = SyntaxKind.AscendingOrdering Then
+                    result.operatorName = StringConstants.OrderByMethod
+                Else
+                    Debug.Assert(ordering.Kind = SyntaxKind.DescendingOrdering)
+                    result.operatorName = StringConstants.OrderByDescendingMethod
+                End If
+
+               result.operatorNameLocation = GetQueryOperatorNameSpan(orderBy.OrderKeyword, orderBy.ByKeyword)
+            Else
+                If ordering.Kind = SyntaxKind.AscendingOrdering Then
+                    result.operatorName = StringConstants.ThenByMethod
+                Else
+                    Debug.Assert(ordering.Kind = SyntaxKind.DescendingOrdering)
+                    result.operatorName = StringConstants.ThenByDescendingMethod
+                End If
+
+                result.operatorNameLocation = orderByOrderings.GetSeparator(i - 1).Span
+            End If
+            Return result
+        End function
 
     End Class
 
