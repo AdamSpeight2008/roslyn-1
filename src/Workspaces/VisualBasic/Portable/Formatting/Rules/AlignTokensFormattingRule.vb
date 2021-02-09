03d24931fcd88618e6a6c10038525d0d1b9a0730
AdamSpeight2008 <adam.speight@virgin.net>
2021-02-09 12:29:53 +0000

    Alter how Select Case block structure, and its contents are aligned. By aligning the `Case` in `Case clause` to the `Select Case expression` `Case`.

--- src/Workspaces/VisualBasic/Portable/Formatting/Rules/AlignTokensFormattingRule.vb
+++ src/Workspaces/VisualBasic/Portable/Formatting/Rules/AlignTokensFormattingRule.vb
@@ -6,6 +6,7 @@ Imports Microsoft.CodeAnalysis.Formatting.Rules
 Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
 
 Namespace Microsoft.CodeAnalysis.VisualBasic.Formatting
+
     Friend Class AlignTokensFormattingRule
         Inherits BaseFormattingRule
         Friend Const Name As String = "VisualBasic Align Tokens Formatting Rule"
@@ -15,17 +16,41 @@ Namespace Microsoft.CodeAnalysis.VisualBasic.Formatting
 
         Public Overrides Sub AddAlignTokensOperationsSlow(operations As List(Of AlignTokensOperation), node As SyntaxNode, ByRef nextOperation As NextAlignTokensOperationAction)
             nextOperation.Invoke()
-
-            Dim queryExpression = TryCast(node, QueryExpressionSyntax)
-            If queryExpression IsNot Nothing Then
-                Dim tokens = New List(Of SyntaxToken)()
-                tokens.AddRange(queryExpression.Clauses.Select(Function(q) q.GetFirstToken(includeZeroWidth:=True)))
-
-                If tokens.Count > 1 Then
-                    AddAlignIndentationOfTokensToBaseTokenOperation(operations, queryExpression, tokens(0), tokens.Skip(1))
-                End If
-                Return
-            End If
+            Select Case node.Kind
+                   Case SyntaxKind.QueryExpression
+                        AlignQueryExpressionClauses(operations, TryCast(node, QueryExpressionSyntax))
+                   Case SyntaxKind.SelectBlock
+                        AlignCaseBlocksToCaseInCaseBlockStatement(operations, TryCast(node, SelectBlockSyntax))
+            End Select
         End Sub
+
+        Private Shared Sub AlignQueryExpressionClauses(operations As List(Of AlignTokensOperation), queryExpression As QueryExpressionSyntax)
+            ' eg
+            ' Dim results = From x In xs
+            '               Where x IsNot Nothing
+            '               Select x
+            '
+            If queryExpression Is Nothing Then Return
+            Dim tokens As New List(Of SyntaxToken)(queryExpression.Clauses.Select(Function(q) q.GetFirstToken(includeZeroWidth:=True)))
+
+            If tokens.Count <= 1 Then Return
+            AddAlignIndentationOfTokensToBaseTokenOperation(operations, queryExpression, tokens(0), tokens.Skip(1))
+        End Sub
+
+        Private Shared Sub AlignCaseBlocksToCaseInCaseBlockStatement(operations As List(Of AlignTokensOperation), caseBlock As SelectBlockSyntax)
+            'eg.
+            '  Select Case expr
+            '         Case 0
+            '         Case Is < 1
+            '         Case Is >= 1
+            '         Case Else
+            '  End Select
+            '            
+            Dim tokens As New List(Of SyntaxToken)(caseBlock.CaseBlocks.Select(Function(q) q.GetFirstToken(includeZeroWidth:=True)))
+            If tokens.Count <= 1 Then Return
+            AddAlignIndentationOfTokensToBaseTokenOperation(operations, caseBlock, caseBlock.SelectStatement.CaseKeyword, tokens)
+        End Sub
+
     End Class
+
 End Namespace
